a75c1a1619df2765aec3fc09e21e6718e312fa21
Keeriya <keeriyabanavathu@gmail.com>
2026-08-06 20:35:23 +0530

    Implement user registration and login functions

---
+++ controllers/authController.js
@@ -0,0 +1,45 @@
+const authService = require('../service/authService')
+async function register(req,res){
+    try{
+        const user = await authService.register(req.body);
+        res.status(210).json({
+            success:true,
+            message:"user registered successfully",
+            data:user
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+async function login(req,res){
+    try{
+        const user = await authService.login(req.body);//bob@gmail.com bob
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message:"invaild email or password"
+            })
+        }
+        res.status(200).json({
+            success:true,
+            message:"login succesfull",
+            data:user
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+module.exports = {register,
+    login
+}
