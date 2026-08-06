40fe7e775330478f6d2d79d3d9bc50db8ff504c7
Keeriya <keeriyabanavathu@gmail.com>
2026-08-06 22:20:53 +0530

    Refactor authController to use MySQL and dotenv
    Refactor authController to use MySQL and dotenv for configuration. Removed register function and updated login function.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,45 +1,16 @@
-const authService = require('../service/authService')
-async function register(req,res){
-    try{
-        const user = await authService.register(req.body);
-        res.status(210).json({
-            success:true,
-            message:"user registered successfully",
-            data:user
-        })
-    }
-    catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message
-        })
-    }
-}
+const mysql = require("mysql2")
+const dotenv = require("dotenv")
 
-async function login(req,res){
-    try{
-        const user = await authService.login(req.body);//bob@gmail.com bob
+dotenv.config()
 
-        if(!user){
-            return res.status(401).json({
-                success: false,
-                message:"invaild email or password"
-            })
-        }
-        res.status(200).json({
-            success:true,
-            message:"login succesfull",
-            data:user
-        });
-    }
-    catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message
-        })
-    }
-}
+const db = mysql.createPool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
+});
 
-module.exports = {register,
-    login
-}
+module.exports = db.promise()//async/await
